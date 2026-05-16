4646077ad263e53a0acafac97c0557c357b9ebab
Anush Shiwakoti <anushshiwakoti43@gmail.com>
2026-05-16 22:03:15 +0545

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,17 @@
+public class datatypes  {
+    public static void main(String[] args) {
+
+        int age = 19;
+        double height = 5.8;
+        boolean student = true;
+        char grade = 'A';
+        String name = "Anush";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);ssss
+        System.out.println("Student: " + student);
+        System.out.println("Grade: " + grade);
+    }
+}
+
